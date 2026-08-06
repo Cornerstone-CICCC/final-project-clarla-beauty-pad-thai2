d1bf8197ecd3cc92292f4ad8117ad422eb18fbf2
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-08-05 19:19:27 -0700

    add second service page

--- script/contact.js
+++ script/contact.js
@@ -1,4 +1,5 @@
 $(function () {
+  //Add border and check icon
   $("#wedding").on("focus", function () {
     $("#wedding").addClass("border-pink bg-check");
     $("#party").removeClass("border-pink bg-check");
@@ -17,6 +18,17 @@ $(function () {
     $("#photoshoot").addClass("border-pink bg-check");
   });
 
+  $("#makeup").on("focus", function () {
+    $("#hairstyle").removeClass("border-pink bg-check");
+    $("#makeup").addClass("border-pink bg-check");
+  });
+
+  $("#hairstyle").on("focus", function () {
+    $("#makeup").removeClass("border-pink bg-check");
+    $("#hairstyle").addClass("border-pink bg-check");
+  });
+
+  // Show phone number / email once at a time
   $("#call").on("click", function () {
     $("#callNumber").removeClass("invisible").addClass("visible");
     $("#whatsNumber").removeClass("visible").addClass("invisible");
@@ -43,5 +55,25 @@ $(function () {
     $("#whatsNumber").removeClass("visible").addClass("invisible");
     $("#smsNumber").removeClass("visible").addClass("invisible");
     $("#callNumber").removeClass("visible").addClass("invisible");
+  });
+
+  // Subtract extra person
+  $(".sub-btn").on("click", function () {
+    const target = $(this).data("target");
+    let count = Number($(target).text()) || 0;
+
+    if (count > 0) {
+      count -= 1;
+      $(target).text(count);
+    }
+  });
+
+  // Add extra person
+  $(".add-btn").on("click", function () {
+    const target = $(this).data("target");
+    let count = Number($(target).text());
+
+    count += 1;
+    $(target).text(count);
   });
 });
